4b2660dd7c908950bd0b585ab472cf3b92132d0f
Olivia Moreau <moreauolivia49@gmail.com>
2022-11-14 14:57:34 +0100

    feat: new component tasks, with CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import Router from "@koa/router";
 import exemplesRoutes from '#components/exemple/exemple-route.js'
+import tasksRoutes from "#components/tasks/tasks-route.js";
 
 const API_V1_ROUTER = new Router({prefix: '/api/v1'})
 
 API_V1_ROUTER.use('/exemples', exemplesRoutes.routes(), exemplesRoutes.allowedMethods())
+API_V1_ROUTER.use('/tasks', tasksRoutes.routes(), tasksRoutes.allowedMethods())
 
 export { API_V1_ROUTER }
